1bd43955ffd60116a8a28a338b26bc1f33508d2d
Francis Githae <francis.githae@student.moringaschool.com>
2021-12-21 06:42:57 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import {Goal} from './goal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  // goal='Watch Friends';
+  title='Goals'
+  goals:Goal[]=[
+    {id:1,name:'Watch Friends'},
+    {id:2,name:'Buy cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Splve math homework'},
+    {id:6,name:'PLot my world domination plan'}
+  ];
+
+  // constructor(){
+  //   this.goals=['Watch Friends','Buy cookies','Get new phone case'];
+  // }
 }
