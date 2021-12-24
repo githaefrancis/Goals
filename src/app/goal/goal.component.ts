72855b4be5ac5b29af09bfbc36443c1bc5bf23d4
“Francis <francizgithae@gmail.com>
2021-12-24 15:36:58 +0300

    add form

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -20,9 +20,13 @@ export class GoalComponent implements OnInit {
   toggleDetails(index:any){
     this.goals[index].showDescription=!this.goals[index].showDescription;
   }
-  completeGoal(isComplete:boolean,index:number){
+  deleteGoal(isComplete:boolean,index:number){
     if(isComplete){
-      this.goals.splice(index,1);
+      let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+      if(toDelete){
+        this.goals.splice(index,1);
+      }
+      
     }
   }
   constructor() { }
